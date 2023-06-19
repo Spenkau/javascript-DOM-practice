55671b837b56d9ca5f8df284cb0f1ae5e9d4a388
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-06-19 13:02:07 +0300

    added functional with libs: cleave & parsley

--- main.js
+++ main.js
@@ -3,14 +3,77 @@ const closeModalBtn = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const closeModalLuckBtn = document.querySelector('.lucky-btn');
 const form = document.querySelector('.send-message');
+const buttons = document.querySelectorAll('.button-panel > li > button');
+const illustrations = document.querySelectorAll('.illustration-content');
 const inputs = form.getElementsByTagName('input');
-const submitBtn = document.querySelector('.send');
+const inputCredit = document.getElementById('input-credit-card');
+const inputPhone = document.getElementById('input-phone');
+const inputDate = document.getElementById('input-date');
 
 AOS.init();
 
-console.log(inputs[0].value)
+const cleaveCreditCard = new Cleave(inputCredit, {
+    creditCard: true
+});
 
-for (let i = 0; i < 5; i++) {
+var cleaveDate = new Cleave(inputDate, {
+    date: true,
+    delimiter: '-',
+    datePattern: ['Y', 'm', 'd']
+});
+
+var cleavePhone = new Cleave(inputPhone, {
+    phone: true,
+    phoneRegionCode: 'BY',
+    delimiter: ' ',
+    blocks: [2, 3, 2, 2],
+    prefix:'+375 ',
+    noImmediatePrefix: true
+});
+
+const swiper = new Swiper(
+    '.first-swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 4,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        spaceBetween: 28
+    }
+)
+
+const swiper2 = new Swiper(
+    '.second-swiper', {
+        direction: 'horizontal',
+        loop: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            // bulletActiveClass: '.swiper-pagination-bullet-active'
+        },
+
+        simulateTouch: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+        mousewheel: {
+            sensitivity: 1,
+        },
+    }
+)
+
+for (let i = 0; i < 6; i++) {
     const input = inputs[i];
 
     input.addEventListener('input', () => {
@@ -49,20 +112,20 @@ form.addEventListener('submit', (e) => {
 
     console.log('Message sent!');
 })
-// }
 
 const handleModalClosing = () => {
-    modal.style.display = 'none';
+    modal.classList.add('hidden');
 
     closeModalBtn.removeEventListener('click', handleModalClosing);
 };
 
 const handleModalOpening = () => {
+    console.log('click')
     setTimeout(() => {
 
         closeModalLuckBtn.innerText = 'good luck';
 
-        modal.style.display = 'block';
+        modal.classList.remove('hidden');
 
         closeModalBtn.addEventListener('click', handleModalClosing);
 
@@ -82,9 +145,33 @@ const handleModalOpening = () => {
             closeModalLuckBtn.innerText = 'Thank you!';
 
             setTimeout(handleModalClosing, 1000);
-
         })
     }, 500)
 }
 
-openModalBtn.addEventListener('click', handleModalOpening)
+openModalBtn.addEventListener('click', handleModalOpening);
+
+const setTab = (event) => {
+    const element = event.target;
+    const tabName = element.getAttribute('data-tab');
+
+    const activeBtn = document.querySelector('.button-panel .active');
+    activeBtn.classList.remove('active');
+
+    element.classList.add('active');
+
+    for (let i = 0; i < illustrations.length; i++) {
+        const tabItem = illustrations[i].getAttribute('data-tab');
+
+        if (tabItem !== tabName && tabName !== 'all') {
+            illustrations[i].classList.add('tabHidden');
+        } else {
+            illustrations[i].classList.remove('tabHidden');
+        }
+    }
+};
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', setTab);
+}
+
